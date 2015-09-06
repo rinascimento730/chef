1d96b33846db77c0106a85fee953a7a409c023f3
Satoshi Fujinaga <b.fujinaga@gmail.com>
2015-09-06 22:27:32 +0900

    Mysqlのバージョン指定

--- site-cookbooks/mysql/recipes/default.rb
+++ site-cookbooks/mysql/recipes/default.rb
@@ -6,8 +6,20 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+=begin
+
+case node["platform"]
+when "centos"
+  yum_repository "mysql-5.6" do
+    url "http://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm"
+    action :add
+  end
+end
+=end
+
 package "mysql-server" do
   action :install
+  #ersion "5.6"
 end
 
 service "mysqld" do
